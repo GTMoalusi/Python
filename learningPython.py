83702c0deac99df542e24d40ae5ae8c7307a0af9
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-07-16 00:07:53 +0200

    Learning python is fun

--- learningPython.py
+++ learningPython.py
@@ -1,5 +1,29 @@
-print("hello")
-print("")
-print()
+# Elif Statement
+# temperature = 22
+# if temperature > 30:
+#    print("It's warm")
+#    print("Drink water")
+# elif temperature > 20:
+#    print("It's nice")
+# else:
+#    print("It's cold")
+# print("Done")
 
-print("bye")
+#Ternary Operator
+age = 22
+
+# if age >= 18:
+#    print("Eligible")
+# else:
+#    print("Not eligible")
+
+# if age >= 18:
+#    message = "Eligible"
+# else:
+#    message = "Not eligible"
+
+# print(message)
+
+message = "Eligible" if age >= 18 else "Not eligible" #Ternary Operator
+
+print(message)
