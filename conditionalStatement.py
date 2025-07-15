bea948708541a296669c63d9f8d64dc3332fa54c
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-07-15 23:27:37 +0200

    first commit

---
+++ conditionalStatement.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+   print(f"You are {age} years old and eligible to vote, good luck🗳️")
+else:
+   print(f"You are {age} years old and not eligible to vote yet, come back in a few years")
